f14832003ca9cd454b4cc9c19cefacaa515a6ff7
nekacurry <anneka498@gmail.com>
2022-01-12 14:10:32 -0600

    added router and axios

---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+
+
+function Home(){
+
+  return (
+    <>
+      <h1>yugidraft</h1>
+      <p>Simulate a Yu-Gi-Oh! draft online</p>
+      <button>yo</button>
+    </>
+  )
+}
+
+export default Home
