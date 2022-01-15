22e3aeea9c18bc33046944ca4e2dad458ba72711
Amman Sood <392075+asood92@users.noreply.github.com>
2022-01-14 19:21:48 -0500

    up

--- backend/router.js
+++ backend/router.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const server = require("./server");
 const User = require("./models/user");
@@ -9,7 +8,6 @@ const { getDeck, getPublicDecks } = require("./models/deck");
 mongoose.Promise = global.Promise;
 const router = express.Router();
 
-
 router.post("/api/checkAvailableRooms", function (req, res) {
   if (Object.keys(server.rooms).includes(req.body)) {
     return res.status(500).send("game exists");
@@ -18,62 +16,57 @@ router.post("/api/checkAvailableRooms", function (req, res) {
   return res.end();
 });
 
-
-
 router.post("/api/getPublicDecks", async function (req, res) {
-  const temp = ''
-  var decks = [{
-    isPublic: true,
-    hasSFWCards: false,
-    hasNSFWCards: false,
-    blackList: [],
-    name: '',
-    approved: true,
-    isNSFW: false
-  }];
+  const temp = "";
+  var decks = [
+    {
+      isPublic: true,
+      hasSFWCards: false,
+      hasNSFWCards: false,
+      blackList: [],
+      name: "",
+      approved: true,
+      isNSFW: false,
+    },
+  ];
 
   const allPublicDecks = await Card.distinct("sets.set_name").lean().exec();
   console.log(decks[0]);
-  console.log('allPublicDecks ', allPublicDecks.length);
+  console.log("allPublicDecks ", allPublicDecks.length);
   for (i = 0; i < allPublicDecks.length; i += 1) {
     var deckName = allPublicDecks[i];
     // console.log(deckName.length);
     decks[i] = {
-        isPublic: true,
-        hasSFWCards: false,
-        hasNSFWCards: false,
-        blackList: [],
-        name: deckName,
-        approved: true,
-        isNSFW: false,
+      isPublic: true,
+      hasSFWCards: false,
+      hasNSFWCards: false,
+      blackList: [],
+      name: deckName,
+      approved: true,
+      isNSFW: false,
     };
   }
-  /
   return res.status(200).send(decks);
-
-
 });
 
 router.post("/api/getInitialCards", async function (req, res) {
-
   console.log("body is, ", req.body);
-  const deckName = '2013 Collectible Tins Wave 1';
-
+  const deckName = "2013 Collectible Tins Wave 1";
 
   var cards = [];
 
-  tempCards = await Card.find({ "sets.set_name": "Absolute Powerforce" }).then(results => {
-    var length = results.length;
-    for (i = 0; i < length; i += 1) {
-      cards.push(results[i]);
+  tempCards = await Card.find({ "sets.set_name": "Absolute Powerforce" }).then(
+    (results) => {
+      var length = results.length;
+      for (i = 0; i < length; i += 1) {
+        cards.push(results[i]);
+      }
     }
-  })
-  console.log(cards[0])
-  console.log(cards.length)
+  );
+  console.log(cards[0]);
+  console.log(cards.length);
   return res.json(cards);
 });
-
-
 
 router.get("/api/getCardsFromDeck/:name", async function (req, res) {
   const deckName = req.params.name;
@@ -95,7 +88,7 @@ router.post("/api/getDeck", async function (req, res) {
   const deckName = req.body;
   console.log(deckName);
   try {
-    const deckExists = await Deck.find({name : 'deckName'});
+    const deckExists = await Deck.find({ name: "deckName" });
     // console.log({ deckExists });
     if (deckExists) {
       return res.send("Deck exists!");
@@ -114,10 +107,9 @@ router.get("/api/listDecks", async (req, res) => {
   var deck = [];
   var limited = [];
   console.log("inside list decks");
-  var uniques = await Card.distinct("sets.set_name").lean().exec()
+  var uniques = await Card.distinct("sets.set_name").lean().exec();
 
-      limited.push(uniques[4]);
-
+  limited.push(uniques[4]);
 
   for (let i = 0; i < limited.length; i += 1) {
     console.log(limited.length, i);
@@ -128,7 +120,6 @@ router.get("/api/listDecks", async (req, res) => {
     // console.log(deck);
   }
   return res.json(deck);
-
 });
 
 module.exports = router;
